9eda33ab0d9d6e9d859b0cd7204be4719ed5e047
wchang88 <60525612+wchang88@users.noreply.github.com>
2023-04-05 01:34:04 -0400

    add detokenizer

--- TailoTokenizer.py
+++ TailoTokenizer.py
@@ -3,7 +3,7 @@ from string import punctuation
 
 class TailoTokenizer():
    def __init__(self):
-      self.initial = ['ph', 'p', 
+      self.consonants = ['ph', 'p', 
                       'm', 'b',
                       'tshi', 'tsh', 'tsi', 'ts', 'th','t', 
                       'n', 'l', 
@@ -14,7 +14,7 @@ class TailoTokenizer():
                       'h']
 
    def tokenize(self, word):
-      for onset in self.initial:
+      for onset in self.consonants:
          if word.lower().find(onset) == 0:
             if onset[-1] == 'i':
                return [word[:len(onset)], word[len(onset) - 1:]]
@@ -34,10 +34,50 @@ class TailoTokenizer():
                tokens.extend(self.tokenize(word))
       return tokens
    
-   def tokenize_file(self, input_f, output_f):
-      with open(input_f, 'r') as in_f:
-         with open(output_f, 'w') as out_f:
+   def tokenize_file(self, in_file, out_file):
+      with open(in_file, 'r') as in_f:
+         with open(out_file, 'w') as out_f:
             line = in_f.readline()
             while line:
                out_f.write(" ".join(self.tokenize_sentence(line)))
+               line = in_f.readline()
+
+   def detokenize(self, tokens):
+      i = 0
+      sentence = []
+      dash_found = False
+      while i < len(tokens):
+         if re.search(r'[%s]+' % re.escape(punctuation), tokens[i]):
+            # if the current token is punctuation
+            if '-' in tokens[i]:
+               dash_found = True
+            sentence.append(tokens[i])
+            i += 1
+         else:
+            if tokens[i] in self.consonants:
+               # if the current token is a consonant, combine it with the next
+               if tokens[i][-1] == 'i' and tokens[i+1][0] == 'i':
+                  # reduce double i into single i
+                  sentence.append("".join([tokens[i], tokens[i+1][1:]]))
+               else:
+                  sentence.append("".join(tokens[i:i+2]))
+               i += 2
+            else:
+               sentence.append(tokens[i])
+               i += 1
+
+            if dash_found:
+               compound = [sentence.pop() for i in range(3)]
+               sentence.append("".join(compound[::-1]))
+               dash_found = False
+            
+      
+      return " ".join(sentence)
+   
+   def detokenize_file(self, in_file, out_file):
+      with open(in_file, 'r') as in_f:
+         with open(out_file, 'w') as out_f:
+            line = in_f.readline()
+            while line:
+               out_f.write(self.detokenize(line.split()) + '\n')
                line = in_f.readline()
